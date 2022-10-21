b561e4228bb053da347a0df7d8531e99d3bb4d4b
KIRAN M R <iamthekiranmr@gmail.com>
2022-10-21 22:21:35 +0530

    Updates

---
+++ precache-manifest.ba7d7a8865a7fbdc606cb8021a5fefc6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "915142984e796e3a62d07baaf6152326",
+    "url": "/MemoriesReact/index.html"
+  },
+  {
+    "revision": "7120a080342fa15a108e",
+    "url": "/MemoriesReact/static/css/main.9afd702f.chunk.css"
+  },
+  {
+    "revision": "3b05ac548bbfe48adccd",
+    "url": "/MemoriesReact/static/js/2.faf17c9e.chunk.js"
+  },
+  {
+    "revision": "eca373d6688a5d5efead34244f081e8b",
+    "url": "/MemoriesReact/static/js/2.faf17c9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7120a080342fa15a108e",
+    "url": "/MemoriesReact/static/js/main.47db8488.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/MemoriesReact/static/js/main.47db8488.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f9880f1145e601dc0ab",
+    "url": "/MemoriesReact/static/js/runtime-main.1feee4ae.js"
+  },
+  {
+    "revision": "8e9763ff2bd5c0571f6be83d85310d40",
+    "url": "/MemoriesReact/static/media/memories-Logo.8e9763ff.png"
+  },
+  {
+    "revision": "6c7b5ec0184b26d4b93f36ec813156b3",
+    "url": "/MemoriesReact/static/media/memories-Text.6c7b5ec0.png"
+  }
+]);
